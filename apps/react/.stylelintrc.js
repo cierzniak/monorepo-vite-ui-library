14ed8f0b953ff081172c465cb7769a2d3d407ff3
Paweł Cierzniakowski <pcierzniakowski@workforcesoftware.com>
2022-06-11 21:04:05 +0200

    add stylelint and config all apps and packages

---
+++ apps/react/.stylelintrc.js
@@ -0,0 +1,5 @@
+module.exports = {
+  extends: [
+    '@my-namespace/stylelint-config/base',
+  ],
+};
---
+++ packages/ui/.stylelintrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  customSyntax: 'postcss-scss',
+  extends: [
+    '@my-namespace/stylelint-config/base',
+  ],
+};
